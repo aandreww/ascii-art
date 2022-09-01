393ec023c4d31a32b997a3e51c3b2d072dbce23e
hexxione <hexxione@gmail.com>
2022-09-01 09:40:03 -0500

    fixed spacing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("                      /^--^\\     /^--^\\     /^--^\\\n" +
+        System.out.println("            /^--^\\     /^--^\\     /^--^\\\n" +
                 "                      \\____/     \\____/     \\____/\n" +
                 "                     /      \\   /      \\   /      \\\n" +
                 "                    |        | |        | |        |\n" +
