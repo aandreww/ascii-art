4abf870e09835d4cfc4051f80fbbef8d510da0e5
hexxione <hexxione@gmail.com>
2022-09-01 09:32:45 -0500

    removed sample code, replaced with an ascii cat

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  /\\_/\\  (\n" +
+                           " ( ^.^ ) _)\n" +
+                           "   \\\"/  (\n" +
+                           " ( | | )\n" +
+                           "(__d b__)");
+    }
+}
